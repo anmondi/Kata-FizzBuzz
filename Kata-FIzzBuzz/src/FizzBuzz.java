2c3b91a45b5d116c7d0d7dc30d39915acca3e446
dam122 <anmondi@gmail.com>
2017-04-05 11:37:42 +0200

    FizzBuzz de uno devuelve uno

--- Kata-FIzzBuzz/src/FizzBuzz.java
+++ Kata-FIzzBuzz/src/FizzBuzz.java
@@ -1,4 +1,4 @@
-    /*
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -8,13 +8,11 @@
  *
  * @author dam122
  */
-public class KataFIzzBuzz {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class FizzBuzz {
     
+    
+    
+    public int getFizzBuzz(int num){
+        return 1;
+    }
 }
